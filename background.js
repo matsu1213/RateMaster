64b7a243e041f49fa13d790e6f5ac0bf3de7d9a5
matsu1213 <matsu1213mgal@gmail.com>
2024-01-25 17:08:05 +0900

    update(2.0)

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+let injectedTabs = new Set()
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (
     //changeInfo.url &&
@@ -5,7 +7,9 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     (tab.url.startsWith("https://twitter.com/i/communitynotes") ||
       tab.url.startsWith("https://twitter.com/i/birdwatch"))
   ) {
-    console.log("injected");
+    if (injectedTabs.has(tabId)) {
+      return;
+    }
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       const activeTab = tabs[0];
       chrome.scripting.executeScript({
@@ -13,24 +17,47 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         files: ["./main.js"],
       });
     });
+    injectedTabs.add(tabId);
   }
 });
 
-/*chrome.webNavigation.onCommitted.addListener(function (details) {
-  console.log(details.tabId);
-  chrome.tabs.get(details.tabId, function(tab) {
-    console.log(tab.url);
-    if (tab.url.startsWith("https://twitter.com/i/communitynotes") ||
-        tab.url.startsWith("https://twitter.com/i/birdwatch")
-    ) {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.scripting.executeScript({
-          target: { tabId: details.tabId },
-          files: ["./main.js"],
-        });
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+  const { tabId } = details;
+  injectedTabs.delete(tabId);
+});
+
+/*
+let isProcessing = false;
+chrome.webRequest.onCompleted.addListener(
+  (details) => {
+    if (details.method === "GET" && !isProcessing) {
+      isProcessing = true
+      console.log("GET request completed:", details.url);
+      fetch(details.url)
+        .then(response => {
+          console.log(response.status);
+
+          //chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+          //  chrome.tabs.sendMessage(tabs[0].id, { msg: "noteDetails"}, details)
+          //})
+          isProcessing = false;
+        })
+        .catch(error => {
+          console.error("Error fetching response text:", error)
+          isProcessing = false;
       });
     }
-  });
-});
+  },
+  { urls: ['https://twitter.com/i/api/*BirdwatchFetchNotes*'] }
+);
 */
+
+
+chrome.commands.onCommand.addListener(function(command) {
+  if (command === "rateNote") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { msg: "rateFirst"})
+    })
+  }
+});
 
--- main.js
+++ main.js
@@ -2,18 +2,53 @@ onLoadPage();
 
 var t;
 var added = [];
+var clicked = [];
+var noteDataArray = []
+
+var switch1 = true
+var switch1Setting1 = true
+var switch1Setting2 = true
+var switch1Setting3 = false
+var switch1Setting4 = true
+var switch1Setting5 = false
+var switch1Setting6 = false
+var switch2 = true
+var switch3 = true
+
+loadFromSettings()
+
+function loadFromSettings(){
+  chrome.storage.local.get(["switch1", "switch1-setting1", "switch1-setting2", "switch1-setting3", "switch1-setting4", "switch1-setting5", "switch1-setting6", "switch2", "switch3"], function(value) {
+    switch1 = value.switch1 ?? true
+    switch1Setting1 = value.switch1Setting1 ?? true
+    switch1Setting2 = value.switch1Setting2 ?? true
+    switch1Setting3 = value.switch1Setting3 ?? false
+    switch1Setting4 = value.switch1Setting4 ?? false
+    switch1Setting5 = value.switch1Setting5 ?? false
+    switch1Setting6 = value.switch1Setting6 ?? false
+    switch2 = value.switch2 ?? true
+    switch3 = value.switch3 ?? true
+  })
+}
 
 function onLoadPage() {
-  //setTimeout(addListener, 3000);
-  //window.addEventListener('popstate', onLoadFromCache);
 
   const observer = new MutationObserver((records) => {
     records.forEach((record) => {
       Array.from(record.addedNodes)
+        .filter(tag => tag.nodeType == 1)
         .forEach((element) => {
-          removeIfAlreadyRated(element)
+          if(switch3){
+            removeIfAlreadyRated(element)
+          }
           if(isNoteSection(element) && isNote(element) && added.indexOf(element) == -1){
-            addListener(element, false)
+            if(switch1){
+              addListener(element, false)
+              notes = element.querySelectorAll('[data-testid="ratingStatus"]')
+              for (const note of notes){
+                displayInfo(note)
+              }
+            }
             added.push(element)
           }
         });
@@ -24,10 +59,7 @@ function onLoadPage() {
     childList: true,
     subtree: true,
   });
-}
 
-function onLoadFromCache(){
-  addListener(document, true)
 }
 
 function addListener(element, cache) {
@@ -41,9 +73,13 @@ function addListener(element, cache) {
       if (chil.innerText === "Yes" || chil.innerText === "はい") {
         chil.addEventListener(
           "click",
-          function () {
+          function a() {
+            if(clicked.indexOf(chil) != -1){
+              return
+            }
+            //console.log("click")
+            clicked.push(chil)
             if(t){ clearTimeout(t) }
-            console.log("click!");
             t = setTimeout(function () {
               parent = chil.closest(".css-175oi2r.r-g2wdr4.r-nsbfu8.r-1xfd6ze")
               if(parent == null){ parent = chil.closest(".css-175oi2r.r-g2wdr4.r-nsbfu8") }
@@ -67,15 +103,110 @@ function run(parent) {
 
   //console.log(e);
 
-  e[0].click();
-  e[1].click();
-  e[3].click();
+  if(switch1Setting1){
+    e[0].click();
+  }
+  if(switch1Setting2){
+    e[1].click();
+  }
+  if(switch1Setting3){
+    e[2].click();
+  }
+  if(switch1Setting4){
+    e[3].click();
+  }
+  if(switch1Setting5){
+    e[4].click();
+  }
+  if(switch1Setting6){
+    e[5].click();
+  }
 
   submit = parent.querySelector(
     ".css-175oi2r.r-sdzlij.r-1phboty.r-rs99b7.r-lrvibr.r-1kihuf0.r-2yi16.r-1qi8awa.r-ymttw5.r-1loqt21.r-o7ynqc.r-6416eg.r-1ny4l3l"
   );
   submit.click();
-  added.splice(0)
+  if(switch2){
+    setTimeout(checkAlreadyRatedAndBack, 500)
+  }
+  added = []
+}
+
+chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
+  if (data.msg === 'rateFirst') {
+    e = document.querySelector('.css-175oi2r.r-g2wdr4.r-nsbfu8.r-1xfd6ze')
+    if(e){
+      buttons = e.querySelectorAll(
+        "div.css-175oi2r.r-sdzlij.r-1phboty.r-rs99b7.r-lrvibr.r-791edh.r-id7aif.r-15ysp7h.r-4wgw6l.r-ymttw5.r-1loqt21.r-o7ynqc.r-6416eg.r-1ny4l3l, div.css-175oi2r.r-sdzlij.r-1phboty.r-rs99b7.r-lrvibr.r-791edh.r-id7aif.r-15ysp7h.r-4wgw6l.r-ymttw5.r-1loqt21.r-o7ynqc.r-6416eg.r-1ny4l3l, div.css-175oi2r.r-sdzlij.r-1phboty.r-rs99b7.r-lrvibr.r-791edh.r-id7aif.r-15ysp7h.r-4wgw6l.r-ymttw5.r-1loqt21.r-o7ynqc.r-6416eg.r-1ny4l3l"
+      );
+      if(buttons != null){
+        for (const chil of buttons) {
+          if (chil.innerText === "Yes" || chil.innerText === "はい") {
+            chil.click()
+          }
+        }
+      }
+    }
+  }else if(data.msg == 'noteDetails'){
+    console.log("noteDetails")
+  }
+  return true
+});
+
+var injectScript = () => {
+  var script = document.createElement('script');
+  script.setAttribute('src', chrome.runtime.getURL('injected.js'));
+  (document.head || document.documentElement).appendChild(script);
+  script.remove();
+};
+injectScript()
+
+window.addEventListener('note_load', (e) => {
+  noteDataArray.push(...(e.detail.notes))
+  setTimeout(applyToDom, 100)
+})
+
+function applyToDom(){
+  nodes = document.querySelectorAll('[data-testid="ratingStatus"]')
+  var count = 0
+  if(nodes){
+      for(const e of nodes){
+          displayInfo(e)
+
+          count = count + 1
+      }
+  }
+}
+
+function displayInfo(e){
+  timeEle = e.querySelector('time')
+  noteInfo = getNoteInfo(new Date(Date.parse(timeEle.dateTime)).getTime())
+  if(noteInfo == null){
+    return
+  }
+  e.childNodes[1].appendChild(e.childNodes[1].childNodes[2].cloneNode())
+  desc = document.createElement('span')
+  desc.innerText = noteInfo.alias + " 作成ポイント" + noteInfo.writingImpacts + " 評価ポイント" + noteInfo.ratingImpacts
+  desc.style = 'text-overflow: unset; color: rgb(113, 118, 123); font-size: 13px;'
+  desc.className = "css-1qaijid r-bcqeeo r-qvutc0 r-1tl8opc r-xoduu5 r-1q142lx r-1w6e6rj r-9aw3ui r-3s2u2q"
+  e.childNodes[1].appendChild(desc)
+}
+
+function getNoteInfo(time){
+//  console.log(noteDataArray)
+  for(const n of noteDataArray){
+    //console.log(n)
+      if(n.created_at == time){
+          return n
+      }
+  }
+  return null
+}
+
+function checkAlreadyRatedAndBack(){
+  if(document.querySelectorAll('.css-175oi2r.r-g2wdr4.r-nsbfu8.r-1xfd6ze').length == 0){
+    window.history.back()
+  }
 }
 
 function isNoteSection(element) {
@@ -113,7 +244,7 @@ function removeIfAlreadyRated(element){
       target = targetDiv.querySelector('span')
       if(target != null && (target.innerText === "「役に立った」と評価しました" || target.innerText === "You rated as helpful")){
       tweet.remove()
-      console.log('deleted!')
+      //console.log('deleted!')
     }
     }
   
